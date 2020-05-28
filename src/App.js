af34331d2ff0e15577f09c192d3b6edb021aa4a4
alisheem <alisheemkhan@gmail.com>
2020-05-28 22:28:33 +0530

    added shop page and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Switch , Route} from 'react-router-dom';
 import Homepage from './Pages/Homepage.component';
+import ShopPage from './Pages/shop.component';
 import './App.css';
+
 
 function App() {
   return (
-    <Homepage />
+    <Switch>
+      <Route exact path='/' component={Homepage}/>
+      <Route exact path='/shop' component={ShopPage}/>
+    </Switch>
   );
 }
 
